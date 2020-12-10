dc1d16fe86df55d5d13dcb8ccde3e7bc455c62c8
Sunil Skanda <skanda1395@gmail.com>
2020-12-10 11:15:31 +0530

    working version of 3 designs (responsive)

--- Task -1/main.js
+++ Task -1/main.js
@@ -1,7 +1,16 @@
 const dropdown_btn = document.querySelector("#dropdown-trigger");
 const dropdown_content = document.querySelector("#dropdown-content");
 
+const coll_menu_icon = document.querySelector("#menu-icon");
+const nav_links = document.querySelector("#nav-links");
+
 dropdown_btn.addEventListener("click", () => {
   let displayProp = dropdown_content.style.display;
   dropdown_content.style.display = displayProp == "block"? "none": "block";
+});
+
+
+coll_menu_icon.addEventListener("click", () => {
+  let displayProp = nav_links.style.display;
+  nav_links.style.display = displayProp == "flex"? "none": "flex";
 });
