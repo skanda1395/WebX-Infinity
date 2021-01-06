b34585b2564f8c99f120c462517f5aa2cac95a3b
Sunil Skanda <skanda1395@gmail.com>
2021-01-06 16:17:28 +0530

    organized folder structure

---
+++ Ramneesh Project/Admin/admin_panel/app.js
@@ -0,0 +1,7 @@
+let toggle = document.querySelector(".toggle");
+let navigation = document.querySelector(".navigation");
+
+function toggleMenu() {
+  navigation.classList.toggle("active");  
+  toggle.classList.toggle("active");  
+}
